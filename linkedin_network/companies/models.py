6cf4d5b881575500281097f08f6be571ae8bd09f
artemdou <ardoumeni@gmail.com>
2024-09-28 18:23:09 +0200

    customize admin to showcase company overview

--- linkedin_network/companies/models.py
+++ linkedin_network/companies/models.py
@@ -6,15 +6,16 @@ class Company(models.Model):
     linkedin_link = models.URLField(max_length=200, verbose_name="LinkedIn Link")
     company_type = models.CharField(max_length=50)
     industry = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     employee_count = models.IntegerField()
     year_founded = models.IntegerField()
 
     # to be scraped
-    investment_stage = models.CharField(max_length=50)
-    funding =  models.IntegerField()
+    headquarters = models.CharField(max_length=50, blank=True, null=True) # this should not be nullanble 
+    investment_stage = models.CharField(max_length=50, blank=True, null=True)
+    funding =  models.IntegerField(blank=True, null=True)
 
 
     def __str__(self):
-         return f'{self.name} - {self.current_company}'
+         return f'{self.name}'
 
--- linkedin_network/connections/admin.py
+++ linkedin_network/connections/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
 from .models import Connection
 
+from django.utils.html import format_html
+from django.urls import reverse
+
 class ConnectionAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'current_company', 'current_position', 'current_location')
     search_fields = ('first_name', 'last_name', 'current_company', 'current_position', 'current_location')
     list_filter = ('current_company', 'current_location')
+
+
+    def current_company_link(self, obj):
+        if obj.current_company:
+            url = reverse('admin:companies_company_change', args=[obj.current_company.id])
+            return format_html('<a href="{}">{}</a>', url, obj.current_company.name)
+        return '-'
+    
+    current_company_link.short_description = 'Current Company'
+    current_company_link.admin_order_field = 'current_company'
 
 admin.site.register(Connection, ConnectionAdmin)
