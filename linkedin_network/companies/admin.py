8cdb2e14b0f342e195c1246cbdc598117c5e59b7
artemdou <ardoumeni@gmail.com>
2024-09-28 17:55:42 +0200

    create admin for companies

--- linkedin_network/companies/admin.py
+++ linkedin_network/companies/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Company
 
-# Register your models here.
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'company_type', 'industry', 'employee_count', 'year_founded')
+    search_fields = ('name', 'industry', 'company_type')
+    list_filter = ('company_type', 'industry', 'year_founded')
+
+admin.site.register(Company, CompanyAdmin)
