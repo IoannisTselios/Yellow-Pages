54c29023628cb69725879a4a2aaec355b6af4b21
artemdou <ardoumeni@gmail.com>
2024-09-28 18:22:10 +0200

    add companies as fk

--- linkedin_network/connections/models.py
+++ linkedin_network/connections/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from companies.models import Company 
 
 class Connection(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     url = models.URLField(max_length=200)
-    current_company = models.CharField(max_length=100)
+    current_company = models.ForeignKey(Company, on_delete=models.SET_NULL, blank=True, null=True)  # ForeignKey to Company model
     current_position = models.CharField(max_length=100)
     current_location = models.CharField(max_length=100)
 
