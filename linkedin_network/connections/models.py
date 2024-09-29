e5ce4f3a0e8d9093922db22e9f46384ba5ea08ae
artemdou <ardoumeni@gmail.com>
2024-09-29 19:38:35 +0200

    add bio and summary fields

--- linkedin_network/connections/models.py
+++ linkedin_network/connections/models.py
@@ -6,6 +6,8 @@ from .utils import strength_metric_calculation
 class Connection(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    bio = models.TextField(null=True, blank=True)
+    summary = models.TextField(null=True, blank=True)
     url = models.URLField(max_length=200)
     location = models.CharField(max_length=100)
 
