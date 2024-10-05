431ee41f9f8362667b83eb564fdea8be3082717d
ntina10 <frenadin@hotmail.com>
2024-10-05 21:21:04 +0200

    fixed forbidden on login

--- linkedin_network/linkedin_network/settings.py
+++ linkedin_network/linkedin_network/settings.py
@@ -155,3 +155,9 @@ CORS_ALLOWED_ORIGINS = [
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
+
