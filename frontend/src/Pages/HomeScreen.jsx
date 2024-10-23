002301256664a42712a08bc060b0caf4d47222e7
ntina10 <frenadin@hotmail.com>
2024-10-23 16:09:09 +0200

    init home page

--- frontend/src/Pages/HomeScreen.jsx
+++ frontend/src/Pages/HomeScreen.jsx
@@ -1,6 +1,8 @@
-import React, { useEffect, useState } from "react"
-import { SubmitButton } from "../Components/Buttons"
+import React, { useEffect, useState } from "react";
+import styles from './HomeScreen.module.css';
 import { Navigate } from 'react-router-dom';
+import { Button } from "@mui/material";
+import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
 
 export const HomeScreen = () => {
   const [redirect, setRedirect] = useState(false);
@@ -8,21 +10,22 @@ export const HomeScreen = () => {
   const [name, setName] = useState('')
 
   useEffect(() => {
-    // (
-    //   async () => {
-    //     const response = await fetch('http://localhost:8000/api/get_current_user', {
-    //       headers: {
-    //         'Content-Type': 'application/json',
-    //       },
-    //       credentials: 'include',
-    //     });
+    (
+      async () => {
+        const response = await fetch('http://localhost:8000/api/get_current_user', {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          credentials: 'include',
+        });
 
-    //     const content = await response.json();
+        const content = await response.json();
 
-    //     setName(content.name);
-    //   }
+        setName(content.first_name);
+        // console.log('Here is the name', content.first_name)
+      }
      
-    // )();
+    )();
   })
 
   const handleLogout = async () => {
@@ -39,7 +42,6 @@ export const HomeScreen = () => {
         throw new Error('Login failed');
       }
 
-      // If logout is successful, handle response (e.g., redirect, save token)
       const data = await logout.json();
 
       console.log('Logout successful:', data);
@@ -56,10 +58,72 @@ export const HomeScreen = () => {
     return <Navigate to='/login' replace />;
   }
 
+  const getData = async () => {
+    try {
+      const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OSwiZXhwIjoxNzI5Njg0MjAzLCJpYXQiOjE3Mjk2ODA2MDN9.8DPnmDqejPWz_zaQqYWPO5HPfalp2ExB3wSk3hwM1_Y"; 
+
+      const response = await fetch('http://localhost:8000/api/get_connection_list/', {
+        // headers: {
+        //   'Content-Type': 'application/json',
+        // },
+        // credentials: 'include',
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+      });
+     
+      if (!response.ok) {
+        throw new Error('Data fetch failed');
+      }
+
+      const data = await response.json();
+
+      console.log('Data fetch successful:', data);
+
+    } catch (error) {
+      console.error('Error during data fetch:', error);
+    }
+
+  } 
+
+  const rows = [
+    { id: 1, col1: 'Bety Boo', col2: 'Jack, Henry, Mary', col3: 'Student', col4: 'Developer', col5: 'Junior', col6: 'Novo Nordisk', col7: 'Pharmaceutical' },
+    { id: 2, col1: 'John Hex', col2: 'Henry, Mary', col3: 'Data Analyst', col4: 'Data Science', col5: 'Mid', col6: 'Hexagon', col7: 'Sales' },
+    { id: 3, col1: 'Arthur Peterson', col2: 'Henry', col3: 'Change Management', col4: 'Communication', col5: 'Junior', col6: 'Novo Nordisk', col7: 'Pharmaceutical' },
+    { id: 4, col1: 'Moby Dick', col2: 'Mary', col3: 'Product Owner', col4: 'Management', col5: 'CEO', col6: 'DontKnow', col7: 'Pharmaceutical' },
+  ];
+
+  const columns = [
+    { field: "id", headerName: '#', hide: true, width: 50 },
+    { field: 'col1', headerName: 'Name', width: 150},
+    { field: 'col2', headerName: 'Connections', width: 150 },
+    { field: 'col3', headerName: 'Position', width: 150 },
+    { field: 'col4', headerName: 'Function', width: 150 },
+    { field: 'col5', headerName: 'Seniority', width: 150 },
+    { field: 'col6', headerName: 'Company', width: 150 },
+    { field: 'col7', headerName: 'Industry', width: 150 },
+  ];
+
   return (
     <div>
-        <p>HOME PAGE</p>
-        <SubmitButton label='Logout' onClick={handleLogout}></SubmitButton>
+      <div className={styles.header}>
+        <div className={styles.logoWrapper}>
+          <img src="Assets\image.png" alt="Logo" />
+          <div className={styles.text}>Yellow Pages</div>
+        </div>
+        <Button variant="contained" color='secondary' size='large' onClick={handleLogout}> Logout </Button>
+      </div>
+      <div className={styles.underline}></div>
+      <div className={styles.mainContent}>
+  
+        <DataGrid style={{ borderRadius: '10px', background: '#fff'}} rows={rows} columns={columns} checkboxSelection disableRowSelectionOnClick/>
+
+        
+
+
+      </div>
     </div>
+    //     <Button variant="contained" color='primary' size='large' onClick={handleLogout}> Logout </Button>
   );
 }
