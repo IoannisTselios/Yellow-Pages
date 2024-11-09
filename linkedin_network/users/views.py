780d81abd2382b00cbd5cfe9a9f77bb572337aac
artemdou <ardoumeni@gmail.com>
2024-11-09 13:37:56 +0100

    change cookie

--- linkedin_network/users/views.py
+++ linkedin_network/users/views.py
@@ -38,7 +38,7 @@ class LoginView(APIView):
 
         response = Response()
 
-        response.set_cookie(key='jwt', value=token, httponly=True, secure=False, samesite='None')
+        response.set_cookie(key='jwt', value=token, httponly=True)
         response.data = {
             'jwt': token
         }
